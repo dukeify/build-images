9baec30dda44575480fe137f5bd8552e8b01ca8e
matthewacon <matthewjbarichello@gmail.com>
2020-08-30 22:29:56 -0400

    Switched to archlinux for CI

--- scripts/setup-host.sh
+++ scripts/setup-host.sh
@@ -8,23 +8,20 @@ if [[ ! -n "$TARGET_GROUP" ]]; then
 fi
 
 #Install docker
-apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable"
-apt update -y
-apt install -y docker-ce docker-ce-cli containerd.io
+yay -Sy --noconfirm --sudoloop docker
+sudo dockerd 2>&1 > /dev/null &
 
 #Enable desired architectures
 #TODO other groups will require special logic
-cd docker/$TARGET_GROUP
-for image in $(ls); do
- target_arch=$(echo "$image" | cut -d- -f1)
- update-binfmts --enable "qemu-$target_arch"
-done
+#cd docker/$TARGET_GROUP
+#for image in $(ls); do
+# target_arch=$(echo "$image" | cut -d- -f1)
+# update-binfmts --enable "qemu-$target_arch"
+#done
 
 #Start and login to docker
-systemctl start docker
-~/login_to_dockerhub.sh
+#systemctl start docker
+sudo ~/login_to_dockerhub.sh
 
 #Return to previous CWD
 cd "$PREVIOUS_CWD"
