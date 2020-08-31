1d46ece4fccda7ff89355b56cb072085396094a7
matthewacon <matthewjbarichello@gmail.com>
2020-08-31 13:51:25 -0400

    Fixed typo in docker start script

--- scripts/common/start_docker.sh
+++ scripts/common/start_docker.sh
@@ -2,7 +2,7 @@
 export LOGGED_IN_TO_DOCKERHUB=1
 
 function is_docker_running() {
- if [ ! -n "$(ps -a | grep -i docker | awk '{ print $4 }')" ]; then
+ if [ -n "$(ps -a | grep -i docker | awk '{ print $4 }')" ]; then
   echo 0
  else
   echo 1
@@ -18,6 +18,7 @@ function assert_docker_is_running() {
 
 if [[ "$(is_docker_running)" == 1 ]]; then
  #Start docker and confirm that the daemon has started
+ printf 'Starting docker\n'
  sudo dockerd 2>&1 > /dev/null &
  assert_docker_is_running
  #Login to dockerhub if the secret is present
@@ -25,4 +26,6 @@ if [[ "$(is_docker_running)" == 1 ]]; then
   export LOGGED_IN_TO_DOCKERHUB=0
   sudo ~/login_to_dockerhub.sh
  fi
+else
+ printf 'Docker is already running'
 fi
