7c5dc6668a943e36e0cdee7cd13eef4b2e0a0d4a
matthewacon <matthewjbarichello@gmail.com>
2020-08-30 21:09:00 -0400

    Renamed sr.ht CI folder, added target group to setup-host script

--- scripts/setup-host.sh
+++ scripts/setup-host.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+PREVIOUS_CWD="$(pwd)"
+TARGET_GROUP="$1"
+if [[ ! -n "$TARGET_GROUP" ]]; then
+ printf 'You must supply the desired group to configure for:\n%s\n' "$(ls docker/)"
+ exit -1
+fi
+
 #Enable desired architectures
-for image in $(ls docker/linux/); do
+#TODO other groups will require special logic
+cd docker/$TARGET_GROUP
+for image in $(ls); do
  target_arch=$(echo "$image" | cut -d- -f1)
  update-binfmts --enable "qemu-$target_arch"
 done
@@ -9,3 +18,6 @@ done
 #Start and login to docker
 systemctl start docker
 ~/login_to_dockerhub.sh
+
+#Return to previous CWD
+cd "$PREVIOUS_CWD"
